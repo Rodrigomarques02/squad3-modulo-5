de96c4a5bc9fdcd83122a00e17a1e90dbe721b47
jeffrazek <jeffrazec97@gmail.com>
2023-09-14 02:26:23 -0300

    Criado arquivos INSERT SQL através de função após tratamento de dados

--- create-db-template.sql
+++ create-db-template.sql
@@ -39,3 +39,94 @@ CREATE TABLE coin_XRP (
     Volume FLOAT,
     Marketcap FLOAT
 );
+
+CREATE TABLE coin_DOT (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_BNB (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_SOL (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_ADA (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_DOGE (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_UNI (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
+
+CREATE TABLE coin_USDC (
+    SNo INT,
+    Name VARCHAR,
+    Symbol VARCHAR,
+    Date TIMESTAMP,
+    High FLOAT,
+    Low FLOAT,
+    Open FLOAT,
+    Close FLOAT,
+    Volume FLOAT,
+    Marketcap FLOAT
+);
